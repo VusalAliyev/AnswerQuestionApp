aad63dbd5ff553492437da3bb2af364b4051d57b
VusalAliyev <vusalaliev02@gmail.com>
2022-10-01 01:39:18 +0400

    WorkingOnHomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,14 +3,25 @@ import "../HomePage/homePage.css"
 import Popup  from './Popup/Popup'
 
 const HomePage = ({Logout,user}) => {
-    const[btnPopup,setBtnPopup]=useState(false)
+    const[btnPopup,setBtnPopup]=useState(false) 
+    const [question, setQuestion] = useState({
+      questionId: "",
+      questionContent: "",
+    });
   return (
     <div className="Home">
     <div className="header">
-        <button onClick={Logout}>Logout</button>
+        <button onClick={Logout} className="logout">Logout</button>
         <p>Coding is easy</p>
-        <button onClick={()=>setBtnPopup(true)}>Ask Question</button>
-        <Popup trigger={btnPopup} setTrigger={setBtnPopup}><h3>Popup</h3></Popup>
+        <button onClick={()=>setBtnPopup(true)} className="askHome">Ask Question</button>
+        <Popup question={question} setQuestion={setQuestion} trigger={btnPopup} setTrigger={setBtnPopup}><h3>Popup</h3></Popup>
+    </div>
+    <div className="questionBox">
+      <div className="questionBox-inner">
+        <div className="questionBoxTop">
+            <p>{question.questionContent}</p>
+        </div>
+      </div>
     </div>
     </div>
   )
--- src/components/HomePage/Popup/Popup.jsx
+++ src/components/HomePage/Popup/Popup.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import "../Popup/popup.css"
+import React, { useState } from "react";
+import "../Popup/popup.css";
+import HomePage from "../HomePage";
 const Popup = (props) => {
-    return (props.trigger) ? (
-        <div className='popup'>
-            <div className="popup-inner">
-                <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
-            </div>
-        </div>
-    ) : ""
-}
+  
+  console.log(props.question);
+  const PopupSubmitHandler = (e) => {
+    e.preventDefault();
+  };
 
-export default Popup
+  return props.trigger ? (
+    <form  className="popup">
+      <div className="popup-inner">
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          X
+        </button>
+        <input
+          onChange={(e) =>
+            props.setQuestion({ ...props.question, questionContent: e.target.value })
+          }
+          value={props.question.questionContent}
+          type="text"
+          placeholder="Write your question..."
+          className="inputPopup"
+        />
+        <button className="submitPopup" onClick={PopupSubmitHandler}>
+          Submit
+        </button>
+      </div>
+    </form>
+  ) : (
+    ""
+  );
+};
+
+export default Popup;
